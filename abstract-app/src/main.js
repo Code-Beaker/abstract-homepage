16a10ee627c59e764e8ef3c13e9230721426714b
Tharun Raj <tharun.2.linux@gmail.com>
2025-08-20 15:07:25 +0530

    Refactor code structure for improved readability and maintainability

--- abstract-app/src/main.js
+++ abstract-app/src/main.js
@@ -1,2 +1,22 @@
+import {
+  buildSrc,
+  buildTransformationString,
+  upload,
+  getResponsiveImageAttributes,
+} from "@imagekit/javascript";
 import "./style.css";
 import "boxicons";
+
+// import { buildTransformationString } from "@imagekit/javascript";
+const transformationString = buildTransformationString([
+  { width: 300, height: 300 },
+  { overlay: { type: "text", text: "Hello World" } },
+]);
+console.log(transformationString);
+// Expected output: "w-300,h-300:l-text,i-Hello%20World,l-end"
+
+buildSrc({
+  urlEndpoint: "https://imagekit.io/dashboard/url-endpoints/lksjdf7sd",
+  src: "https://ik.imagekit.io/yfkxwgc35/cyberheads/hero-home.jpg?updatedAt=1728301437571",
+  transformation: [{ width: 300, height: 300 }, { blur: 10 }],
+});
